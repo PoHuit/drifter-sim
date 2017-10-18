bfe9e0c3c47cff7993a6a1dffa3570d91439039c
Po Huit <eve.po.huit@gmail.com>
2017-10-18 01:12:19 -0700

    replaced buggy connection algorithm

--- drifter-sim.py
+++ drifter-sim.py
@@ -17,21 +17,17 @@ def sim():
             observatories[obs].add(system)
     # Filter out noise for efficiency.
     connections = [s for s in observatories if len(s) >= 2]
-    # Goofy version of Warshall's Algorithm.
-    cspan = set()
-    for src in range(5):
-        for dst in range(src + 1, 5):
-            for c in connections:
-                if src in c and dst in c:
-                    for csrc, cdst in set(cspan):
-                        if cdst == src:
-                            cspan.add((csrc, dst))
-                    cspan.add((src, dst))
+    # Transitive closure
+    closure = {0}
+    running = True
+    while running:
+        running = False
+        for c in connections:
+            if c & closure != {} and not (c <= closure):
+                closure = closure | c
+                running = True
     # Check connectivity.
-    for system in range(4):
-        if (system, system+1) not in cspan:
-            return False
-    return True
+    return closure == set(range(5))
 
 n = 10000
 c = 0
